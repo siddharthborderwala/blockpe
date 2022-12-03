42420aaf5ce5897d22460d8134654b5780fcf8a5
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2022-12-03 22:19:23 +0530

    add Payment Link and QRCode code

--- pages/payment-link.js
+++ pages/payment-link.js
@@ -1,6 +1,28 @@
-import { Box, GradientFullBgLayout } from '~/components/GradientFullBgLayout';
+import { GradientFullBgLayout } from '~/components/GradientFullBgLayout';
+
+import { Box, Flex, Link, Image, Divider, Text } from '@chakra-ui/react';
+import { useRef, useEffect } from 'react';
+import QRCode from 'qrcode';
+
+const opts = {
+  errorCorrectionLevel: 'H',
+  type: 'image/jpeg',
+  quality: 0.3,
+  color: {
+    light: '#FFF',
+  },
+};
 
 const PaymentLink = () => {
+  const qrCodeRef = useRef(null);
+  const paymentLink = 'https://blockpe.com/123';
+
+  useEffect(() => {
+    QRCode.toDataURL(paymentLink, opts, function (_, url) {
+      qrCodeRef.current && (qrCodeRef.current.src = url);
+    });
+  }, []);
+
   return (
     <GradientFullBgLayout>
       <Box
@@ -11,7 +33,71 @@ const PaymentLink = () => {
         width="75vw"
         height="75vh"
         experimental_spaceY="4"
-      ></Box>
+      >
+        <Flex justifyContent="space-between">
+          <Box width="100%">
+            <Text as="strong" fontSize="1.3rem">
+              Name:{' '}
+            </Text>
+            <Text>BlockPe</Text>
+
+            <Text as="strong" fontSize="1.3rem">
+              Description:{' '}
+            </Text>
+            <Text>This is BlockPe</Text>
+
+            <Flex justifyContent="space-between">
+              <Box>
+                <Text as="strong" fontSize="1.3rem">
+                  Token:{' '}
+                </Text>
+                <Text>ETH</Text>
+              </Box>
+
+              <Box>
+                <Text as="strong" fontSize="1.3rem">
+                  Amount:{' '}
+                </Text>
+                <Text>0.001</Text>
+              </Box>
+            </Flex>
+          </Box>
+          <Flex
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            padding="4rem"
+          >
+            <Link
+              href={paymentLink}
+              isExternal
+              fontSize="1.5rem"
+              marginBottom="2rem"
+              color="blue.700"
+            >
+              {paymentLink}
+            </Link>
+
+            <Flex alignItems="center">
+              <Divider width="12rem" />
+              <Text margin="0 1rem" color="gray.600">
+                or
+              </Text>
+              <Divider width="12rem" />
+            </Flex>
+
+            <Box width="15rem" height="15rem">
+              <Image
+                src=""
+                alt="qr-code"
+                ref={qrCodeRef}
+                height="100%"
+                width="100%"
+              />
+            </Box>
+          </Flex>
+        </Flex>
+      </Box>
     </GradientFullBgLayout>
   );
 };
